3d42af7f594099529e482a1fe1276fcbc4b2066e
nhithy303 <tranhoangyennhi303@gmail.com>
2023-05-04 13:50:14 +0700

    Update 'matching-game' with dynamic data

--- src/app/controllers/PlayController.js
+++ src/app/controllers/PlayController.js
@@ -1,20 +1,30 @@
-// const Course = require('../models/Course');
-// const { multipleMongooseToObject, mongooseToObject } = require('../../util/mongoose');
+const MatchingGame = require('../models/MatchingGame');
+const { multipleMongooseToObject, mongooseToObject } = require('../../util/mongoose');
 
 class PlayController {
 
     // [GET] /play
     index(req, res, next) {
         res.render('user/play/play', {
+            title: "Chơi mà học |",
             user: req.session.username,
         });
     }
 
     // [GET] /play/matching-game
     matchingGame(req, res, next) {
-        res.render('user/play/matching-game', {
-            user: req.session.username,
-        });
+        // res.render('user/play/matching-game', {
+        //     user: req.session.username,
+        // });
+        MatchingGame.findOne({ topic: 'animal' })
+            .then(matchinggame =>
+                res.render('user/play/matching-game', {
+                    title: "Matching Game |",
+                    user: req.session.username,
+                    matchinggame: mongooseToObject(matchinggame),
+                }),
+            )
+            .catch(next);
     }
     
 }

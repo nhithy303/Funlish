dfb2cd21110d571452ea5641d4f4ab2806683bd4
nhithy303 <tranhoangyennhi303@gmail.com>
2024-05-30 01:26:56 +0700

    Fix secret key

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose');
-const connectionStr = "mongodb+srv://funlish:cntt4701104@funlish.ixuxxok.mongodb.net/Funlish?retryWrites=true&w=majority";
+const mongoose = require('mongoose')
+const secret = require('./secret')
+const connectionStr = secret.connectionStr
 
 async function connect() {
-    try {
-        await mongoose.connect(connectionStr, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            // useCreateIndex: true,
-        });
-        console.log('Connect successfully!!!');
-    } catch (error) {
-        console.log(`Connect failure!!!\nError: ${error}`);
-    }
+  try {
+    await mongoose.connect(connectionStr, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    console.log('Connect successfully!!!')
+  } catch (error) {
+    console.log(`Connect failure!!!\nError: ${error}`)
+  }
 }
 
-module.exports = { connect };
+module.exports = { connect }
